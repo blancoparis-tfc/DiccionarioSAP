df08571f45bd0f4e5a6d74b1074fc3c4a2325689
blancoparis-tfc <blancoparis.tfc@gmail.com>
2018-03-20 21:43:33 +0100

    Configuracion basica del proyecto

---
+++ src/main/kotlin/org/dbp/diccionarioSap/DiccionarioSapApplication.kt
@@ -0,0 +1,11 @@
+package org.dbp.diccionarioSap
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiccionarioSapApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiccionarioSapApplication>(*args)
+}
