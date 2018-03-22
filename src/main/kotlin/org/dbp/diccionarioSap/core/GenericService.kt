1ec5138dc79fd69945ed8f7a1855ccb18e269e89
blancoparis-tfc <blancoparis.tfc@gmail.com>
2018-03-22 23:15:08 +0100

    Refaztarizar

--- src/main/kotlin/org/dbp/diccionarioSap/core/GenericService.kt
+++ src/main/kotlin/org/dbp/diccionarioSap/core/GenericService.kt
@@ -1,6 +1,5 @@
 package org.dbp.diccionarioSap.core
 
-import org.springframework.data.crossstore.ChangeSetPersister
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
